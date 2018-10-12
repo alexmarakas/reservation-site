dbb845e4bde3fd44661baaccf87eb38395e767ae
Alex Marakas <alex.marakas@gmail.com>
2018-10-11 20:56:39 -0700

    google maps

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 function initMap(){
- navigator.geolocation.getCurrentPosition(function(position){
-   var userLocation = {
+   var location = {
       lat: 40.8054491,
       lng: -73.9654415
     };
-    
+
    var map = new google.maps.Map(document.getElementById('map'), {
-      center: userLocation,
+      center: location,
       zoom: 10,
       scrollwheel: false
 	});
 
    var marker = new google.maps.Marker({
-     position: userLocation,
+     position: location,
      map: map,
      title: 'User Location'
    });
- });
 }
 
 initMap();
